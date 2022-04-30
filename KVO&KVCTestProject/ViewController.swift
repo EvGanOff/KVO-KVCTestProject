0e03f70a58d7b61b367934733b850127ec26c44a
Evgeny Ganusenko <evgan66@gmail.comevgan66@gmail.comevgan66@gmail.com>
2022-04-30 15:21:19 +0300

    Initial Commit

---
+++ KVO&KVCTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KVO&KVCTestProject
+//
+//  Created by Евгений Ганусенко on 4/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
